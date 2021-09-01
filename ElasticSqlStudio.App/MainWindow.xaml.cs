a60db40256205dfa40c56ea140d2405b5bd35b74
Sascha Werheid <sascha.werheid@de.tuv.com>
2021-09-01 10:42:54 +0200

    add first version of ElasticSqlStudio

---
+++ ElasticSqlStudio.App/MainWindow.xaml.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows;
+using ElasticSqlStudio.App.Annotations;
+using ElasticSqlStudio.Data;
+
+using Microsoft.Win32;
+
+namespace ElasticSqlStudio.App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string url = "http://localhost:9200";
+
+        private string user = "none";
+
+        private string query = "SELECT clientip, request FROM kibana_sample_data_logs";
+
+        private string output;
+
+        private bool isValid;
+
+        private bool hasConnection;
+
+        private ElasticRepository elasticRepository;
+
+        private Visibility progressBarVisibility = Visibility.Collapsed;
+
+        public MainWindow()
+        {
+            this.DataContext = this;
+            InitializeComponent();
+        }
+
+        public string Url
+        {
+            get => this.url;
+            set
+            {
+                if (value == this.url) return;
+                this.url = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public string User
+        {
+            get => this.user;
+            set
+            {
+                if (value == this.user) return;
+                this.user = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public string Password
+        {
+            get => this.PasswordTextBox.Password;
+            set
+            {
+                if (value == this.PasswordTextBox.Password) return;
+                this.PasswordTextBox.Password = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public string Query
+        {
+            get => this.query;
+            set
+            {
+                if (value == this.query) return;
+                this.query = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public string Output
+        {
+            get => this.output;
+            set
+            {
+                this.output = $"---------------------- {DateTime.Now} ---------------------- {Environment.NewLine}{value}";
+                this.OnPropertyChanged();
+            }
+        }
+
+        public bool HasConnection
+        {
+            get => this.hasConnection;
+            set
+            {
+                if (value == this.hasConnection) return;
+                this.hasConnection = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public bool IsValid
+        {
+            get => this.isValid;
+            set
+            {
+                if (value == this.isValid) return;
+                this.isValid = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public Visibility ProgressBarVisibility
+        {
+            get => this.progressBarVisibility;
+            set
+            {
+                if (value == this.progressBarVisibility) return;
+                this.progressBarVisibility = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        private async void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            await Task.Run(
+                async () =>
+                    {
+                        this.ProgressBarVisibility = Visibility.Visible;
+                        try
+                        {
+                            this.elasticRepository = new ElasticRepository(this.Url, this.User, this.Password);
+                            await this.elasticRepository.PingElastic();
+                            this.Output = "Connection valid";
+                            this.HasConnection = true;
+                        }
+                        catch (Exception exception)
+                        {
+                            this.Output = exception.ToString();
+                        }
+
+                        this.ProgressBarVisibility = Visibility.Collapsed;
+                    });
+        }
+
+        private async void ValidateButton_Click(object sender, RoutedEventArgs e)
+        {
+            await Task.Run(
+                async () =>
+                    {
+                        this.ProgressBarVisibility = Visibility.Visible;
+                        try
+                        {
+                            var result = await this.elasticRepository.RunSingleQuery(this.Query);
+                            this.Output = $"Valid Request. First row: {result}";
+                            this.IsValid = true;
+                        }
+                        catch (Exception exception)
+                        {
+                            this.Output = exception.ToString();
+                        }
+
+                        this.ProgressBarVisibility = Visibility.Collapsed;
+                    });
+        }
+
+        private async void ExportButton_Click(object sender, RoutedEventArgs e)
+        {
+            await Task.Run(
+                async () =>
+                    {
+                        this.ProgressBarVisibility = Visibility.Visible;
+                        try
+                        {
+                            var saveFileDialog = new SaveFileDialog();
+                            saveFileDialog.FileName = $"{DateTime.Now.ToFileTimeUtc()}.csv";
+                            if (saveFileDialog.ShowDialog() == true)
+                            {
+                                var result = await this.elasticRepository.QueryComplete(this.Query);
+                                await File.WriteAllTextAsync(saveFileDialog.FileName, result);
+                                this.Output = $"Exportet to {saveFileDialog.FileName}";
+                            }
+                        }
+                        catch (Exception exception)
+                        {
+                            this.Output = exception.ToString();
+                        }
+
+                        this.ProgressBarVisibility = Visibility.Collapsed;
+                    });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ ElasticSqlStudio.Data/ElasticRepository.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Nest;
+
+namespace ElasticSqlStudio.Data
+{
+    public class ElasticRepository
+    {
+        private readonly ElasticClient client;
+
+        public ElasticRepository(string url, string user, string password)
+        {
+            var uri = new Uri(url);
+            var connection = new ConnectionSettings(uri);
+            connection.BasicAuthentication(user, password);
+
+            // tracing elastic, needs more performance so only comment in when needed
+            //connection.DisableDirectStreaming().OnRequestCompleted(details =>
+            //    {
+            //        if (details.RequestBodyInBytes != null)
+            //        {
+            //            Trace.WriteLine($"### ES REQEUST ###{Environment.NewLine}{Encoding.UTF8.GetString(details.RequestBodyInBytes)}, Uri: {details.Uri} Method: {details.HttpMethod}");
+            //        }
+            //        if (details.ResponseBodyInBytes != null)
+            //        {
+            //            Trace.WriteLine($"### ES RESPONSE ###{Environment.NewLine}{Encoding.UTF8.GetString(details.ResponseBodyInBytes)}");
+            //        }
+            //    }).PrettyJson();
+
+            this.client = new ElasticClient(connection); }
+
+        public async Task PingElastic()
+        {
+            var response = await this.client.PingAsync();
+            if (!response.IsValid)
+            {
+                throw response.OriginalException;
+            }
+        }
+
+        public async Task<string> RunSingleQuery(string query)
+        {
+            if (query.Contains("LIMIT"))
+            {
+                throw new ArgumentException("Do not use LIMIT, thx ;)");
+            }
+
+            var request = new QuerySqlRequest { Query = query + " LIMIT 1", Format = "json" };
+            var response = await this.client.Sql.QueryAsync(request);
+
+            if (response.IsValid)
+            {
+                return this.ConvertRows2CSV(response.Rows);
+            }
+
+            throw response.OriginalException;
+        }
+
+        public async Task<string> QueryComplete(string query)
+        {
+            var request = new QuerySqlRequest
+            {
+                Query = query, 
+                Format = "json", FetchSize = 1000
+            };
+
+            var response = await this.client.Sql.QueryAsync(request);
+
+            if (response.IsValid)
+            {
+                var rtnString = this.ConvertRows2CSV(response.Rows);
+                var cursor = response.Cursor;
+                while (!string.IsNullOrEmpty(cursor))
+                {
+                    var cursorResponse = await this.client.Sql.QueryAsync(new QuerySqlRequest { Cursor = response.Cursor });
+                    rtnString += this.ConvertRows2CSV(cursorResponse.Rows);
+                    cursor = cursorResponse.Cursor;
+                }
+
+                return rtnString;
+            }
+            
+            throw response.OriginalException;
+        }
+
+        private string ConvertRows2CSV(IReadOnlyCollection<SqlRow> responseRows)
+        {
+            var rtnString = string.Empty;
+            foreach (var responseRow in responseRows)
+            {
+                foreach (var sqlValue in responseRow)
+                {
+                    try
+                    {
+                        rtnString += $"{sqlValue?.As<string>()};";
+                    }
+                    catch (Exception)
+                    {
+                        rtnString += "CAN'T_PARSE_THIS_AS_STRING;";
+                    }
+                }
+                rtnString += Environment.NewLine;
+            }
+
+            return rtnString;
+        }
+    }
+}
